10e50ffaaa1c8478ee44c9b5e403b13ebc5375ff
Theodore John Josef <john.josef@aol.com>
2023-02-05 01:04:42 -0800

    commit tree 93619ec1c86e5a66d1d3185c6b699a9643befc5d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :spices, only: [:index, :create, :show, :update, :delete] 
+  #get '/spices', to: "spices#index" 
+  #:show is similar to get'/spices/:id', to: "spices#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
